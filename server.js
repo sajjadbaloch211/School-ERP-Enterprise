bec787f960538f60b7191416e747be303bd5473f
Sajjad Ali <sb213284@gmail.com>
2026-06-28 23:03:07 -0700

    Debug env variables

--- server.js
+++ server.js
@@ -51,8 +51,10 @@ const app = express();
 app.set('trust proxy', 1);
 
 // Database Connection (MySQL/XAMPP)
+
 const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
     database: process.env.DB_NAME || 'waqar_school_db',
