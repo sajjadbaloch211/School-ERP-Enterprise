61dd3d9e76fc79470cad9d99c8ffe97737112660
Sajjad Ali <sb213284@gmail.com>
2026-06-28 22:50:21 -0700

    Debug env variables

--- server.js
+++ server.js
@@ -39,12 +39,13 @@ const auditSystem = require('./audit_system');
 
 dotenv.config();
 
-console.log("DB_HOST =", process.env.DB_HOST);
-console.log("DB_PORT =", process.env.DB_PORT);
-console.log("DB_USER =", process.env.DB_USER);
-console.log("DB_PASS =", process.env.DB_PASS ? "FOUND" : "MISSING");
-console.log("DB_NAME =", process.env.DB_NAME);
-
+console.log("===== DB VARIABLES =====");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASS:", process.env.DB_PASS ? "FOUND" : "MISSING");
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("========================");
 
 const app = express();
 app.set('trust proxy', 1);
